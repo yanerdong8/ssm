31b20279f2b36fa12ea9ba7f9310c04a51394612
yanerdong8 <yanerdong8@sina.com>
2017-06-23 16:50:24 +0800

    logincontroller

---
+++ src/main/java/com/yed/system/controller/LoginController.java
@@ -0,0 +1,10 @@
+package com.yed.system.controller;
+
+import org.springframework.stereotype.Controller;
+
+import com.yed.common.controller.BaseController;
+
+@Controller
+public class LoginController extends BaseController{
+	
+}
